5ce5b4fa1d926d93e95e3fbc79e2b0182403f264
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-10-12 11:15:02 -0500

    Se agrega la vista para el manejo de estudiante en la aplicacion

--- src/main/java/uniquindio/preparcial2/viewController/EstudiantesViewController.java
+++ src/main/java/uniquindio/preparcial2/viewController/EstudiantesViewController.java
@@ -1,4 +1,47 @@
 package uniquindio.preparcial2.viewController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class EstudiantesViewController {
+
+    @FXML
+    private TableColumn<?, ?> colCodigo;
+
+    @FXML
+    private TableColumn<?, ?> colNombre;
+
+    @FXML
+    private TableColumn<?, ?> colNotas;
+
+    @FXML
+    private TableView<?> tableEstudiante;
+
+    @FXML
+    private TextField txfCodigo;
+
+    @FXML
+    private TextField txfNombre;
+
+    @FXML
+    private TextField txfNotas;
+
+    @FXML
+    void actualizarEstudiante(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarEstudiante(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarEstudiante(ActionEvent event) {
+
+    }
+
 }
